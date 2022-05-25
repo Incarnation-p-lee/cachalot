3e2ff8dae82a1282f79106c1c4b855bd211388f2
Pan Li <incarnation.p.lee@outlook.com>
2022-05-25 01:30:42 +0000

    Fix comment.
    Signed-off-by: Pan Li <incarnation.p.lee@outlook.com>

--- pkg/assert/assert.go
+++ pkg/assert/assert.go
@@ -10,7 +10,7 @@ func logAndFail(t *testing.T, message string) {
     t.Fail()
 }
 
-// AssertFalse will check actual is the same as expect, or it will fail the test.
+// AssertEqual will check actual is the same as expect, or it will fail the test.
 func AssertEqual(t *testing.T, expect, actual interface{}, message string) {
     if expect == actual {
         return
@@ -26,7 +26,7 @@ func AssertEqual(t *testing.T, expect, actual interface{}, message string) {
     logAndFail(t, msg)
 }
 
-// AssertFalse will check actual is true, or it will fail the test.
+// AssertTrue will check actual is true, or it will fail the test.
 func AssertTrue(t *testing.T, actual bool, message string) {
     if actual {
         return
