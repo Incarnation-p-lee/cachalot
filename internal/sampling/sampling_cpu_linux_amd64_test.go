f598f9bf8a945ec68b50f79f459a7cf80cdbdb51
Pan Li <Incarnation-p-lee@users.noreply.github.com>
2022-06-11 11:58:37 +0800

    Leverage 5 seconds duration diff for cpu usage. (#15)
    Signed-off-by: Pan Li <incarnation.p.lee@outlook.com>

--- internal/sampling/sampling_cpu_linux_amd64_test.go
+++ internal/sampling/sampling_cpu_linux_amd64_test.go
@@ -5,19 +5,44 @@ import (
     "github.com/Incarnation-p-lee/cachalot/pkg/assert"
 )
 
-func TestSampleCPU(t *testing.T) {
+func TestSampleCPUStat(t *testing.T) {
     testPID := 1
-
-    cpuStat := sampleCPU(testPID)
+    cpuStat := sampleCPUStat(testPID)
 
     assert.IsTrue(t, cpuStat.JiffiesInTotal != invalidJiffies,
         "cpu total jiffies should not be invalid.")
     assert.IsTrue(t, cpuStat.JiffiesInTotal != defaultJiffies,
         "cpu total jiffies should not be default value.")
 
-    assert.IsTrue(t, cpuStat.JiffiesUsed != invalidJiffies,
-        "cpu process used jiffies should not be invalid.")
-    assert.IsTrue(t, cpuStat.JiffiesUsed != defaultJiffies,
-        "cpu process used jiffies should not be default value.")
+    assert.IsTrue(t, cpuStat.UsageInPercentage <= 100.0,
+        "the cpu usage percentage should be less than or equal to 100.0")
+}
+
+func TestSampleCPUJiffies(t *testing.T) {
+    testPID := 1
+    totalJiffies, processJiffies := sampleCPUJiffies(testPID)
+
+    assert.IsTrue(t, processJiffies <= totalJiffies,
+        "the process jiffies should be less than or eual to total jiffies")
+
+    assert.IsTrue(t, processJiffies != invalidJiffies,
+        "the process used jiffies should not be invalid.")
+
+    assert.IsTrue(t, processJiffies != defaultJiffies,
+        "the process used jiffies should not be default value.")
+}
+
+func TestTestSampleCPUJiffiesInvalidPID(t *testing.T) {
+    testPID := 10000000
+    totalJiffies, processJiffies := sampleCPUJiffies(testPID)
+
+    assert.IsTrue(t, processJiffies <= totalJiffies,
+        "the process jiffies should be less than or eual to total jiffies")
+
+    assert.IsTrue(t, processJiffies == invalidJiffies,
+        "the invalid process id used jiffies should be invalid.")
+
+    assert.IsTrue(t, processJiffies != defaultJiffies,
+        "the invalid process id used jiffies should not be default value.")
 }
 
