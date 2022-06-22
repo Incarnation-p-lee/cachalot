15fc899e7a762e2212bbe84020e867a71c9a20c1
Restyled.io <commits@restyled.io>
2022-06-22 11:47:07 +0000

    Restyled by gofmt

--- internal/print/print.go
+++ internal/print/print.go
@@ -1,93 +1,92 @@
 package print
 
 import (
-    "fmt"
-    "log"
-    "time"
-    "errors"
-    "encoding/json"
-    "internal/options"
-    "github.com/Incarnation-p-lee/cachalot/pkg/snapshot"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/Incarnation-p-lee/cachalot/pkg/snapshot"
+	"internal/options"
+	"log"
+	"time"
 )
 
 const (
-    jsonPrefix = ""
-    jsonIndent = "  "
-    printSeparatedLine = "=========================================================="
+	jsonPrefix         = ""
+	jsonIndent         = "  "
+	printSeparatedLine = "=========================================================="
 )
 
 func printSnapshotTitle(title string) {
-    if len(title) == 0 {
-        title = "Unknown title"
-    }
+	if len(title) == 0 {
+		title = "Unknown title"
+	}
 
-    fmt.Println(printSeparatedLine)
-    fmt.Printf("%s\n", title)
-    fmt.Println(printSeparatedLine)
+	fmt.Println(printSeparatedLine)
+	fmt.Printf("%s\n", title)
+	fmt.Println(printSeparatedLine)
 }
 
 func printSnapshotTimestamp(timestamp time.Time) {
-    fmt.Printf("Timestamp: %v\n", timestamp)
-    fmt.Println(printSeparatedLine)
+	fmt.Printf("Timestamp: %v\n", timestamp)
+	fmt.Println(printSeparatedLine)
 }
 
 func printSnapshotProcess(process snapshot.Process) {
 
-    fmt.Printf("%v\t\t", process.PID)
-    fmt.Printf("%.3f%%\t\t", process.CPUStat.UsageInPercentage)
-    fmt.Printf("%s\n", process.CmdLine)
+	fmt.Printf("%v\t\t", process.PID)
+	fmt.Printf("%.3f%%\t\t", process.CPUStat.UsageInPercentage)
+	fmt.Printf("%s\n", process.CmdLine)
 }
 
 func printSnapshotProcesses(processes []snapshot.Process) {
-    fmt.Printf("Total procesess count: %d\n", len(processes))
-    fmt.Println(printSeparatedLine)
-    fmt.Printf("PID\t\tCPUUsage\tCmdLine\n")
-    fmt.Println(printSeparatedLine)
+	fmt.Printf("Total procesess count: %d\n", len(processes))
+	fmt.Println(printSeparatedLine)
+	fmt.Printf("PID\t\tCPUUsage\tCmdLine\n")
+	fmt.Println(printSeparatedLine)
 
-    for _, process := range processes {
-        printSnapshotProcess(process)
-    }
+	for _, process := range processes {
+		printSnapshotProcess(process)
+	}
 }
 
 func printSnapshotFoot() {
-    fmt.Printf("\n\n")
+	fmt.Printf("\n\n")
 }
 
 func printTextSnapshot(snapshot snapshot.Snapshot, title string) {
-    printSnapshotTitle(title)
+	printSnapshotTitle(title)
 
-    printSnapshotTimestamp(snapshot.Timestamp)
-    printSnapshotProcesses(snapshot.Processes)
+	printSnapshotTimestamp(snapshot.Timestamp)
+	printSnapshotProcesses(snapshot.Processes)
 
-    printSnapshotFoot()
+	printSnapshotFoot()
 }
 
 func printJSONSnapshot(snapshot snapshot.Snapshot) {
-    // It is not easy to get errors when serialize object to string. Thus, ignore the error.
-    jsonBytes, _ := json.MarshalIndent(snapshot, jsonPrefix, jsonIndent)
+	// It is not easy to get errors when serialize object to string. Thus, ignore the error.
+	jsonBytes, _ := json.MarshalIndent(snapshot, jsonPrefix, jsonIndent)
 
-    fmt.Printf("%s\n", string(jsonBytes))
+	fmt.Printf("%s\n", string(jsonBytes))
 }
 
 // Snapshot will print the data module of given snapshot.
 func Snapshot(snapshot snapshot.Snapshot, title string, ops *options.Options) error {
-    if ops == nil {
-        return errors.New("found nil ops for snapshot print, will do nothing here")
-    }
+	if ops == nil {
+		return errors.New("found nil ops for snapshot print, will do nothing here")
+	}
 
-    outputFormat := ops.GetOutputFormat()
+	outputFormat := ops.GetOutputFormat()
 
-    switch (outputFormat) {
-        case options.TextOutput:
-            printTextSnapshot(snapshot, title)
-        case options.JSONOutput:
-            printJSONSnapshot(snapshot)
-        default:
-            log.Printf("Unknown output format %v, fall back to %+v.\n",
-                outputFormat, options.TextOutput)
-            printTextSnapshot(snapshot, title)
-    }
+	switch outputFormat {
+	case options.TextOutput:
+		printTextSnapshot(snapshot, title)
+	case options.JSONOutput:
+		printJSONSnapshot(snapshot)
+	default:
+		log.Printf("Unknown output format %v, fall back to %+v.\n",
+			outputFormat, options.TextOutput)
+		printTextSnapshot(snapshot, title)
+	}
 
-    return nil
+	return nil
 }
-
--- internal/print/print_test.go
+++ internal/print/print_test.go
@@ -1,56 +1,55 @@
 package print
 
 import (
-    "time"
-    "testing"
-    "internal/options"
-    "internal/cmdflags"
-    "github.com/Incarnation-p-lee/cachalot/pkg/assert"
-    "github.com/Incarnation-p-lee/cachalot/pkg/snapshot"
+	"github.com/Incarnation-p-lee/cachalot/pkg/assert"
+	"github.com/Incarnation-p-lee/cachalot/pkg/snapshot"
+	"internal/cmdflags"
+	"internal/options"
+	"testing"
+	"time"
 )
 
 func TestPrintSnapshotDefault(t *testing.T) {
-    ops := options.CreateOptions()
-    testProcesses := []snapshot.Process {
-        snapshot.Process {},
-    }
-    testSnapshot := snapshot.CreateSnapshot(time.Now(), testProcesses)
+	ops := options.CreateOptions()
+	testProcesses := []snapshot.Process{
+		snapshot.Process{},
+	}
+	testSnapshot := snapshot.CreateSnapshot(time.Now(), testProcesses)
 
-    assert.IsNil(t, Snapshot(testSnapshot, "", ops), "print empty title snapshot should have nil error")
-    assert.IsNil(t, Snapshot(testSnapshot, "abc", ops), "print text title snapshot should have nil error")
-    assert.IsNotNil(t, Snapshot(testSnapshot, "", nil), "print snapshot should have error")
+	assert.IsNil(t, Snapshot(testSnapshot, "", ops), "print empty title snapshot should have nil error")
+	assert.IsNil(t, Snapshot(testSnapshot, "abc", ops), "print text title snapshot should have nil error")
+	assert.IsNotNil(t, Snapshot(testSnapshot, "", nil), "print snapshot should have error")
 
-    cmdflags.ParseOptions(ops)
-    assert.IsNil(t, Snapshot(testSnapshot, "", ops), "print parsed snapshot should have nil error")
+	cmdflags.ParseOptions(ops)
+	assert.IsNil(t, Snapshot(testSnapshot, "", ops), "print parsed snapshot should have nil error")
 }
 
 func TestPrintTextSnapshot(t *testing.T) {
-    ops := options.CreateOptions()
-    testProcesses := []snapshot.Process {
-        snapshot.Process {},
-    }
+	ops := options.CreateOptions()
+	testProcesses := []snapshot.Process{
+		snapshot.Process{},
+	}
 
-    testSnapshot := snapshot.CreateSnapshot(time.Now(), testProcesses)
-    ops.AppendOption(options.Option {
-        Key: options.OutputFormat,
-        Val: options.TextOutput,
-    })
+	testSnapshot := snapshot.CreateSnapshot(time.Now(), testProcesses)
+	ops.AppendOption(options.Option{
+		Key: options.OutputFormat,
+		Val: options.TextOutput,
+	})
 
-    assert.IsNil(t, Snapshot(testSnapshot, "", ops), "print text snapshot should have nil error")
+	assert.IsNil(t, Snapshot(testSnapshot, "", ops), "print text snapshot should have nil error")
 }
 
 func TestPrintJsonSnapshot(t *testing.T) {
-    ops := options.CreateOptions()
-    testProcesses := []snapshot.Process {
-        snapshot.Process {},
-    }
+	ops := options.CreateOptions()
+	testProcesses := []snapshot.Process{
+		snapshot.Process{},
+	}
 
-    testSnapshot := snapshot.CreateSnapshot(time.Now(), testProcesses)
-    ops.AppendOption(options.Option {
-        Key: options.OutputFormat,
-        Val: options.JSONOutput,
-    })
+	testSnapshot := snapshot.CreateSnapshot(time.Now(), testProcesses)
+	ops.AppendOption(options.Option{
+		Key: options.OutputFormat,
+		Val: options.JSONOutput,
+	})
 
-    assert.IsNil(t, Snapshot(testSnapshot, "", ops), "print json snapshot should have nil error")
+	assert.IsNil(t, Snapshot(testSnapshot, "", ops), "print json snapshot should have nil error")
 }
-
