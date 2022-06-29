921625b7296c370fe882092a6c010df25094e601
Pan Li <Incarnation-p-lee@users.noreply.github.com>
2022-06-29 09:26:08 +0800

    Add total memory in KB to sampling init. (#53)
    * Add total memory in KB to sampling init.
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- internal/sampling/sampling.go
+++ internal/sampling/sampling.go
@@ -1,18 +1,21 @@
 package sampling
 
 import (
-    "time"
-    "internal/options"
-    "github.com/Incarnation-p-lee/cachalot/pkg/snapshot"
+	"github.com/Incarnation-p-lee/cachalot/pkg/snapshot"
+	"internal/options"
+	"time"
 )
 
-func Sample(ops *options.Options) snapshot.Snapshot {
-    if ops == nil {
-        return snapshot.Snapshot {}
-    }
-
-    timestamp, processes := time.Now(), sampleAllProcesses(ops)
-
-    return snapshot.CreateSnapshot(timestamp, processes)
+func init() {
+	initTotalMemoryInKB()
 }
 
+func Sample(ops *options.Options) snapshot.Snapshot {
+	if ops == nil {
+		return snapshot.Snapshot{}
+	}
+
+	timestamp, processes := time.Now(), sampleAllProcesses(ops)
+
+	return snapshot.CreateSnapshot(timestamp, processes)
+}
