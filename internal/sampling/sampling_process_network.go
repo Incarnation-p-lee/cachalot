0ec78404b96c060845b30bf325a5ecdf9f089d09
Pan Li <incarnation.p.lee@outlook.com>
2022-07-10 15:20:40 +0000

    Add sampling for process network TCP4.
    Signed-off-by: Pan Li <incarnation.p.lee@outlook.com>

---
+++ internal/sampling/sampling_process_network.go
@@ -0,0 +1,111 @@
+package sampling
+
+import (
+	"errors"
+	"fmt"
+	"github.com/Incarnation-p-lee/cachalot/pkg/snapshot"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	invalidConnectionCount = -1
+	invalidINode = "-1"
+	socketFilePrefix = "'socket:["
+)
+
+var invalidProcessNetworkStat = snapshot.ProcessNetworkStat{
+	TCP4Stat: snapshot.ProcessTCP4Stat {
+		ConnectionCount: invalidConnectionCount,
+		ConnectionCountByState: map[string]int{
+			tcpEstablished: invalidConnectionCount,
+			tcpSynSent: invalidConnectionCount,
+			tcpSynRecv: invalidConnectionCount,
+			tcpFinWait1: invalidConnectionCount,
+			tcpFinWait2: invalidConnectionCount,
+			tcpTimeWait: invalidConnectionCount,
+			tcpClose: invalidConnectionCount,
+			tcpCloseWait: invalidConnectionCount,
+			tcpLastACK: invalidConnectionCount,
+			tcpListen: invalidConnectionCount,
+			tcpClosing: invalidConnectionCount,
+			tcpNewSynRecv: invalidConnectionCount,
+		},
+	},
+}
+
+var emptyProcessNetworkStat = snapshot.ProcessNetworkStat{
+	TCP4Stat: snapshot.ProcessTCP4Stat {
+		ConnectionCount: 0,
+		ConnectionCountByState: map[string]int{
+			tcpEstablished: 0,
+			tcpSynSent: 0,
+			tcpSynRecv: 0,
+			tcpFinWait1: 0,
+			tcpFinWait2: 0,
+			tcpTimeWait: 0,
+			tcpClose: 0,
+			tcpCloseWait: 0,
+			tcpLastACK: 0,
+			tcpListen: 0,
+			tcpClosing: 0,
+			tcpNewSynRecv: 0,
+		},
+	},
+}
+
+func getSocketFileINode(targetFile string) (iNode string, err error) {
+	iNode, err = invalidINode, nil
+
+	if !strings.HasPrefix(targetFile, socketFilePrefix) { // 'socket:[30862686]'
+		err = errors.New(fmt.Sprintf("target file should be started with %s", socketFilePrefix))
+	} else {
+		targetFile = strings.Trim(targetFile, "'")        // socket:[30862686]
+		fileData := strings.Split(targetFile, ":")        // [socket [30862686]]
+
+		if len(fileData) >= 2 {
+			iNode = strings.Trim(fileData[1], "[]")       // 30862686
+		} else {
+			err = errors.New("the size of file data should be greater than or equal to 2 ")
+		}
+	}
+
+	return iNode, err
+}
+
+func sampleProcessNetworkStat(pID int, spshot snapshot.Snapshot,
+	networkStatChan chan<- snapshot.ProcessNetworkStat) {
+	fdDir := fmt.Sprintf("/proc/%d/fd", pID)
+	files, err := ioutil.ReadDir(filepath.Clean(fdDir))
+
+	if err != nil {
+		log.Printf("Failed to read dir from %s due to %+v\n", fdDir, err)
+		networkStatChan<- invalidProcessNetworkStat
+		return
+	}
+
+	count, networkStat := 0, emptyProcessNetworkStat
+
+	for _, file := range files {
+		filename := file.Name()
+		targetFilename, err := os.Readlink(filename)
+
+		if err != nil {
+			log.Printf("Failed to read link from file %s due to %+v\n", filename, err)
+		} else if iNode, err := getSocketFileINode(targetFilename); err == nil {
+			iNodeToTCP4 := spshot.Network.INodeToTCP4
+
+			if tcp4, has := iNodeToTCP4[iNode]; has {
+				networkStat.TCP4Stat.ConnectionCountByState[tcp4.State] += 1
+				count++
+			}
+		}
+	}
+
+	networkStat.TCP4Stat.ConnectionCount = count
+	networkStatChan<- networkStat
+}
+
---
+++ internal/sampling/sampling_process_network_test.go
@@ -0,0 +1,49 @@
+package sampling
+
+import (
+	"github.com/Incarnation-p-lee/cachalot/pkg/assert"
+	"github.com/Incarnation-p-lee/cachalot/pkg/snapshot"
+	"os/exec"
+	"testing"
+)
+
+func TestGetSocketFileINodeWithError(t *testing.T) {
+	_, err := getSocketFileINode("invalid file")
+
+	assert.IsNotNil(t, err, "invalid file will have err")
+
+	_, err = getSocketFileINode("'socket[30862686]'")
+
+	assert.IsNotNil(t, err, "no comma file will have err")
+}
+
+func TestSampleProcessNetworkStatInvalid(t *testing.T) {
+	testStatChan := make(chan snapshot.ProcessNetworkStat)
+
+	go sampleProcessNetworkStat(1000000, snapshot.Snapshot{}, testStatChan)
+
+	stat := <-testStatChan
+
+	assert.IsEqual(t, invalidConnectionCount,  stat.TCP4Stat.ConnectionCount,
+		"invalid pID should have invalid process network stat")
+}
+
+func TestSampleProcessNetworkStat(t *testing.T) {
+	cmd := exec.Command("sleep", "54321")
+	cmd.Start()
+
+	testPID := cmd.Process.Pid
+	testStatChan := make(chan snapshot.ProcessNetworkStat)
+
+	go sampleProcessNetworkStat(testPID, snapshot.Snapshot{}, testStatChan)
+
+	stat := <-testStatChan
+
+	assert.IsTrue(t, invalidConnectionCount != stat.TCP4Stat.ConnectionCount,
+		"valid pID should have valid process network stat")
+
+	for _, count := range stat.TCP4Stat.ConnectionCountByState {
+		assert.IsTrue(t, invalidConnectionCount != count,
+			"valid pID should have valid process network count by state")
+	}
+}
