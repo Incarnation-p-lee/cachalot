44f6c1d2c20548772474b24c0ee4bd7b2597d1b9
Pan Li <panli@microsoft.com>
2022-07-11 15:13:21 +0800

    Resolve code coverage.
    Signed-off-by: Pan Li <panli@microsoft.com>

--- internal/sampling/sampling_process_network_test.go
+++ internal/sampling/sampling_process_network_test.go
@@ -1,9 +1,14 @@
 package sampling
 
 import (
+	"fmt"
 	"github.com/Incarnation-p-lee/cachalot/pkg/assert"
 	"github.com/Incarnation-p-lee/cachalot/pkg/snapshot"
+	"io/ioutil"
+	"path/filepath"
+	"os"
 	"os/exec"
+	"syscall"
 	"testing"
 )
 
@@ -17,6 +22,13 @@ func TestGetSocketFileINodeWithError(t *testing.T) {
 	assert.IsNotNil(t, err, "no comma file will have err")
 }
 
+func TestGetSocketFileINode(t *testing.T) {
+	iNode, err := getSocketFileINode("'socket:[1243]'")
+
+	assert.IsNil(t, err, "valid file will have no err")
+	assert.IsEqual(t, "1243", iNode, "valid file will have the same inode")
+}
+
 func TestSampleProcessNetworkStatInvalid(t *testing.T) {
 	testStatChan := make(chan snapshot.ProcessNetworkStat)
 
@@ -28,15 +40,43 @@ func TestSampleProcessNetworkStatInvalid(t *testing.T) {
 		"invalid pID should have invalid process network stat")
 }
 
+func getProcessFirstSocketFileINode(pID int) string {
+	fdDir := fmt.Sprintf("/proc/%d/fd", pID)
+	files, _ := ioutil.ReadDir(filepath.Clean(fdDir))
+	socketINode := invalidINode
+
+	for _, file := range files {
+		filename := file.Name()
+		targetFilename, _ := os.Readlink(filename)
+
+		if iNode, err := getSocketFileINode(targetFilename); err == nil {
+			socketINode = iNode
+			break
+		}
+	}
+
+	return socketINode
+}
+
 func TestSampleProcessNetworkStat(t *testing.T) {
-	cmd := exec.Command("sleep", "54321")
+	cmd := exec.Command("python3", "-m", "http.server", "9843")
 	cmd.Start()
 
 	testPID := cmd.Process.Pid
 	testStatChan := make(chan snapshot.ProcessNetworkStat)
+	testINode := getProcessFirstSocketFileINode(testPID)
+	testSnapshot := snapshot.Snapshot{
+		Network: snapshot.Network{
+			INodeToTCP4: map[string]snapshot.TCP4Connection {
+				testINode: snapshot.TCP4Connection{
+					INode: testINode,
+					State: "Established",
+				},
+			},
+		},
+	}
 
-	go sampleProcessNetworkStat(testPID, snapshot.Snapshot{}, testStatChan)
-
+	go sampleProcessNetworkStat(testPID, testSnapshot, testStatChan)
 	stat := <-testStatChan
 
 	assert.IsTrue(t, invalidConnectionCount != stat.TCP4Stat.ConnectionCount,
@@ -46,4 +86,6 @@ func TestSampleProcessNetworkStat(t *testing.T) {
 		assert.IsTrue(t, invalidConnectionCount != count,
 			"valid pID should have valid process network count by state")
 	}
+
+	cmd.Process.Signal(syscall.SIGTERM)
 }
